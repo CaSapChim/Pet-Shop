a3983bcbccbecb705484dc08147333dd87aa9fc7
KrySoSad <duythuc070205@gmail.com>
2024-02-25 10:13:56 +0700

    ✅ Upd(Header): Add dropdown menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,13 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faShop, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { faShop, faArrowRight, faCaretDown } from '@fortawesome/free-solid-svg-icons';
 
 interface Item {
+    name: string;
+    link: string;
+}
+interface ItemOfPage {
     name: string;
     link: string;
 }
@@ -14,10 +18,18 @@ export const Header = () => {
         { name: "ABOUT", link: "/about"},
         { name: "SERVICE", link: "/service"},
         { name: "PRODUCT", link: "/product"},
-        { name: "PAGE", link: ""},
+    ]
+
+    const itemsOfPage: ItemOfPage[] = [
+        { name: "Pricing Plan", link: "" },
+        { name: "The Team", link: "" },
+        { name: "Testimonial", link: "" },
+        { name: "Blog Grid", link: "" },
+        { name: "Blog Detail", link: "" },
     ]
 
     const [activeLink, setActiveLink] = useState<string>("Home");
+    const [open, setOpen] = useState<boolean>(false);
 
     return (
         <header className='h-20 w-full flex items-center bg-white border-b'>
@@ -39,6 +51,29 @@ export const Header = () => {
                         </li>
                     ))
                 }
+                <li 
+                    className={`relative ml-10 cursor-pointer`}
+                    onClick={() => setOpen(!open)}
+                >
+                    <p className='flex items-center gap-2 hover:text-primarycolor duration-300 font-bold text-xl'>
+                        PAGE <FontAwesomeIcon icon={faCaretDown} className='w-4 h-4'/>
+                    </p>
+                    <div className={`
+                        absolute bg-white h-fit py-2 w-36 -bottom-[240px] -translate-x-5  shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]
+                        ${open ? "" : "hidden"}
+                    `}>
+                        {
+                            itemsOfPage.map((item, i) => (
+                                <div
+                                    key={i}
+                                    className='hover:bg-gray-300 p-2'
+                                >
+                                    {item.name}
+                                </div>
+                            ))
+                        }
+                    </div>
+                </li>
             </ul>
             <div className='bg-primarycolor h-full w-64 flex justify-center items-center text-white font-bold text-xl ml-20 px-10'>
                 <span className='w-full flex gap-2 items-center cursor-pointer'>CONTACT <FontAwesomeIcon icon={faArrowRight} className='text-sm'/></span>
